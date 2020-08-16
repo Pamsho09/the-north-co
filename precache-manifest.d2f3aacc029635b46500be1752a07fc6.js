fa8408f91348478d2a33c8077ba48652a2f4279b
pamsho09 <francisco@studiolechuga.com>
2020-08-16 00:37:58 -0500

    Updates

---
+++ precache-manifest.d2f3aacc029635b46500be1752a07fc6.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13cea583a2e162c7caeeec99393c1d03",
+    "url": "/the-north-cow/index.html"
+  },
+  {
+    "revision": "16a782ce37709eeab619",
+    "url": "/the-north-cow/static/css/main.c83b0b58.chunk.css"
+  },
+  {
+    "revision": "b5e6c61364b222b0e16e",
+    "url": "/the-north-cow/static/js/2.08073e18.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/the-north-cow/static/js/2.08073e18.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "16a782ce37709eeab619",
+    "url": "/the-north-cow/static/js/main.6258c6e1.chunk.js"
+  },
+  {
+    "revision": "79ddbcbb654b9b3648a8",
+    "url": "/the-north-cow/static/js/runtime-main.89362904.js"
+  },
+  {
+    "revision": "17b55751db7bb60449035c15b7f7a7d5",
+    "url": "/the-north-cow/static/media/carrito-de-compras.17b55751.png"
+  },
+  {
+    "revision": "976fe1be51bc1d9e716f4b33fd493319",
+    "url": "/the-north-cow/static/media/dead.976fe1be.png"
+  },
+  {
+    "revision": "afa8da7c03a66209d6aa51f5bfcf5e5e",
+    "url": "/the-north-cow/static/media/logo.afa8da7c.png"
+  }
+]);
